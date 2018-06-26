e47ffa03153b2452449e36995639cd9a91f8239f
ndntesting <ndntesting@outlook.com>
2018-06-26 18:03:52 +0000

    Initial commit

---
+++ build.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+dotnet restore src/taskasync/*.csproj && dotnet build src/taskasync/*.csproj
---
+++ src/taskasync/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace taskasync
+{
+    public class Class1
+    {
+    }
+}
